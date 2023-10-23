2c73594bc5e6ab49e6656b91372edb2c681952ee
Jeremy Merlin <jeremymerlin@hotmail.fr>
2023-10-23 16:31:38 +0100

    view single article page done

--- src/components/HomeArticle.jsx
+++ src/components/HomeArticle.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomeArticle({ article }) {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate(`/home/${article.article_id}`);
+  }
+
   return (
-    <article className="home-article">
+    <article className="home-article" onClick={() => handleClick()}>
       <div className="img">
         <img src={article.article_img_url} alt="img of the article" />
       </div>
--- src/pages/MyArticle.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function MyArticle() {
-  return <div>MyArticle</div>;
-}
-
-export default MyArticle;
