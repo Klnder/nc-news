0aeedcb90472c8630da67cddd10222f8dd86d8f1
Jeremy Merlin <jeremymerlin@hotmail.fr>
2023-10-30 21:00:06 +0000

    add loading message when waiting db

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,31 +13,43 @@ function Home() {
   const navigate = useNavigate();
   const [sortBy, setSortBy] = useState(searchParams.get("sortBy") || "created_at");
   const [order, setOrder] = useState(searchParams.get("order") || "desc");
+  const [isLoading, setIsLoading] = useState(true);
 
   async function fetchArticles() {
     try {
       const articlesTemp = await getArticles(topic, sortBy, order);
       setArticles(articlesTemp);
+      setIsLoading(false);
     } catch (err) {
       navigate(`/error/${err.response.data.msg}`);
     }
   }
 
   useEffect(() => {
+    console.log(isLoading);
     setSearchParams({ sortBy: sortBy, order: order });
     fetchArticles();
   }, [topic, sortBy, order]);
 
   return (
     <>
-      <h2>Menu: </h2>
-      <NavTopic />
-      <SortMenu order={order} setOrder={setOrder} sortBy={sortBy} setSortBy={setSortBy} />
-      <div id="home-container">
-        {articles.map((article) => {
-          return <HomeArticle article={article} key={article.article_id} />;
-        })}
-      </div>
+      {isLoading && (
+        <div id="loading-container">
+          <p>Loading Data from API, should not be long !</p>
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <h2>Menu: </h2>
+          <NavTopic />
+          <SortMenu order={order} setOrder={setOrder} sortBy={sortBy} setSortBy={setSortBy} />
+          <div id="home-container">
+            {articles.map((article) => {
+              return <HomeArticle article={article} key={article.article_id} />;
+            })}
+          </div>
+        </>
+      )}
     </>
   );
 }
